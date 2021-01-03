b63db722e12bb7487c06cf3ea24c80ea32e573b3
Vaxyyy <60188922+Vaxyyy@users.noreply.github.com>
2021-01-03 19:39:29 +1100

    Update reconnectButton.js

--- modules/reconnectButton.js
+++ modules/reconnectButton.js
@@ -1,88 +1,71 @@
 (function () {
-	var vtMod, getValues, bar, data, window_body_orig;
+    var rcMod, topButton, window_body_orig;
 
-	eval(onecup['import']());
+    eval(onecup['import']());
 
-	vtMod = window.valueTrackerMod = {};
+    rcMod = window.reconnectMod = {};
 
-	vtMod.draw = function () {
-		return bar();
-	};
+    rcMod.valid_uiModes = ['battle', 'multiplayer', 'design', 'battleroom', 'quickscore'];
 
-	vtMod.players = {};
+    topButton = function (mode, fn) {
+        return div(".hover-black", function () {
+            display("inline-block");
+            height(64);
+            width(64);
+            position("relative");
+            img({
+                src: "https://raw.githubusercontent.com/Vaxyyy/istrolidUI/main/" + mode + ".png",
+                width: 44,
+                height: 44
+            }, function () {
+                top(0);
+                left(10);
+                return position("absolute");
+            });
+            div(function () {
+                position("absolute");
+                line_height(12);
+                font_size(12);
+                text_align("center");
+                width(64);
+                top(44);
+                return text(mode);
+            });
+            if (fn != null) {
+                return fn();
+            } else {
+                if (ui.mode === mode) {
+                    background_color("rgba(255,255,255,.6)");
+                    return onclick(function () {
+                        return ui.go('battle');
+                    });
+                } else {
+                    return onclick(function () {
+                        return ui.go(mode);
+                    });
+                }
+            }
+        });
+    };
 
-	getValues = function () {
-		for (let i = 0; i < sim.players.length; i++) {
-			let player = sim.players[i]
-			let p = vtMod.players[player.number] = player;
-			p.unitValue = 0;
-			p.eco = 10; 
-		};
+    window_body_orig = window.body;
 
-		for (let i in intp.things) {
-			let thing = intp.things[i],
-				player;
-			if (thing.commandPoint) {
-				for (let i in vtMod.players) {
-					let p = vtMod.players[i];
-					if (p.side == thing.side) player = p;
-				};
-				if (!player) continue;
-				player.eco++;
-			}
-		};
-
-		for (let i in intp.things) {
-			let thing = intp.things[i],
-				player;
-			if (vtMod.players[thing.owner]) player = vtMod.players[thing.owner];
-			if (!player || thing.side !== player.side) continue;
-			if (thing.unit) player.unitValue += thing.cost;
-		};
-	};
-
-	bar = function () {
-		return div(function () {
-			background_color('rgba(0,0,0,.3)');
-			transform('translate(-50%, 0%)');
-			display("inline-block");
-			position("relative");
-			left('50%');
-			height(112);
-			width(400);
-			top(64);
-			return data()
-		});
-	};
-
-	data = function () {
-		if (sim.serverType == "1v1" || sim.serverType == "1v1r" || sim.serverType == "1v1t" || sim.serverType == "2v2" || sim.serverType == "3v3") {
-			for (let i in vtMod.players) {
-				let player = vtMod.players[i];
-				if (player.side == "spectators") continue;
-				div(function () {
-					
-					if (player.name == commander.name) {
-						color('white')
-					} else {
-						color('rgba(255,255,255,.6)')
-					}
-					text(`${player.name} | money ${player.money} | value ${player.unitValue} | eco ${player.eco}`)
-				});
-			};
-		}
-	};
-
-	setInterval(() => {
-		getValues()
-	}, 125);
-
-	window_body_orig = window.body;
-
-	window.body = function () {
-		if (ui.mode === 'battle') {
-			vtMod.draw();
-		}
-		return window_body_orig.call(this);
-	};
+    window.body = function () {
+        if (rcMod.valid_uiModes.includes(ui.mode)) {
+            div(function () {
+                position("absolute");
+                right(128);
+                top(0);
+                z_index('2');
+                color("white");
+                return topButton("reconnect", fn = function() { 
+                    onclick(function () {
+                        if (network.websocket !== undefined) network.websocket.close();
+                        return battleMode.joinServer(battleMode.serverName);
+                    })
+                });
+            });
+        }
+        return window_body_orig.call(this);
+    };
 }).call(this);
