472eac73bf730ce53aa03fac8ae695dbcdb67b17
Vaxyyy <60188922+Vaxyyy@users.noreply.github.com>
2021-01-03 15:59:07 +1100

    Update reconnectButton.js

--- modules/reconnectButton.js
+++ modules/reconnectButton.js
@@ -1,52 +1,61 @@
-function drawReconnectButton() {
+(function () {
+    var rcMod, topButton, window_body_orig;
+
     eval(onecup['import']());
 
-    css('.recImg', function () {
-        position('absolute');
-        left(10);
-        top(0);
-    });
-    css('.recText', function () {
-        position('absolute');
-        line_height(12);
-        font_size(12);
-        text_align('center');
-        width(64);
-        top(44);
-        color('white');
-    });
+    white = [255, 255, 255, 255];
 
-    div('.hover-black', () => {
-        position('absolute');
-        text_align('right');
-        right(128);
-        top(0);
-        z_index('10');
-        height(64);
-        width(64);
-        img({
-            src: "img/ui/reload.png",
-            width: 44,
-            height: 44,
-            class: 'recImg'
+    rcMod = window.reconnectMod = {};
+
+    rcMod.valid_uiModes = ['battle', 'multiplayer', 'design', 'battleroom', 'quickscore'];
+
+    topButton = function (mode, fn) {
+        return div(".hover-black", function () {
+            display("inline-block");
+            height(64);
+            width(64);
+            position("relative");
+            img({
+                src: "https://raw.githubusercontent.com/Vaxyyy/istrolidUI/main/" + mode + ".png",
+                width: 44,
+                height: 44
+            }, function () {
+                top(0);
+                left(10);
+                return position("absolute");
+            });
+            div(function () {
+                position("absolute");
+                line_height(12);
+                font_size(12);
+                text_align("center");
+                width(64);
+                top(44);
+                return text(mode);
+            });
+            if (fn != null) {
+                return fn();
+            } else {
+                return onclick(function () {
+                    if (network.websocket !== undefined) network.websocket.close();
+                    return battleMode.joinServer(battleMode.serverName);
+                });
+            }
         });
-        div('.recText', () => {
-            text('reconnect');
-        });
-        onclick(e => reconnect());
-    });
-}
+    };
 
-function reconnect() {
-    if (network.websocket !== undefined) network.websocket.close();
-    return battleMode.joinServer(battleMode.serverName);
-}
+    window_body_orig = window.body;
 
-let window_body_orig = window.body;
-
-window.body = function () {
-    if (ui.mode === 'battle' || ui.mode === 'multiplayer' || ui.mode === 'design' || ui.mode === 'battleroom' || ui.mode === 'quickscore') {
-        drawReconnectButton();
-    }
-    return window_body_orig.call(this);
-};
+    window.body = function () {
+        if (rcMod.valid_uiModes.includes(ui.mode)) {
+            div(function () {
+                position("absolute");
+                right(128);
+                z_index('2');
+                color("white");
+                return topButton("reconnect");
+            });
+        }
+        return window_body_orig.call(this);
+    };
+}).call(this);
