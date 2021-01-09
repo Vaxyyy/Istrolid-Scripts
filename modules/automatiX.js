6c18abab788d7f97ec650c7a5e46d5df19fd9e87
Vaxyyy <60188922+Vaxyyy@users.noreply.github.com>
2021-01-09 16:27:21 +1100

    Update automatiX.js

--- modules/automatiX.js
+++ modules/automatiX.js
@@ -3,7 +3,7 @@
 
     autoX = window.automatiX  = {};
 
-    autoX.enabled = false;
+	autoX.enabled = false;
 
     autoX.intv = null;
 
@@ -16,16 +16,15 @@
         clearInterval(intv);
         return autoX.intv = setInterval(() => {
             if (autoX.enabled && sim.state === 'running' && commander.side !== 'spectators') {
+                commander.selection = [];
                 for (let i in intp.things) {
                     unit = intp.things[i];
                     if (unit.name === 'Unit' && unit.side === commander.side) {
                         commander.selection.push(unit);
                     }
                 }
-                return invt = setTimeout (() => {
-                    network.send("stopOrder");
-                    commander.selection = [];
-                }, autoX.time / 8 );
+                network.send("stopOrder");
+                commander.selection = [];
             }
         }, autoX.time);
     };
