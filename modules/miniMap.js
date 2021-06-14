bb9cf7f848f204f5f3f7d7c3de2598f4d4cff25d
Vaxyyy <60188922+Vaxyyy@users.noreply.github.com>
2021-06-14 13:24:10 +1000

    Create miniMap.js

---
+++ modules/miniMap.js
@@ -0,0 +1,208 @@
+//miniMap
+// open mini map "M"
+(function () {
+    eval(onecup["import"]());
+
+    environment = {
+        mapColor_RGB: "rgba(0,0,0,255)",
+        miniMap_type: "cost",
+        miniMap_enabled: true,
+        valueTracker_enabled: false
+    };
+    sim.map_hight = 0;
+    sim.map_width = 0;
+
+    miniButton = function (name, binding, fn) {
+        return div(".hover-black", function () {
+            position("absolute");
+            height(42);
+            width(42);
+            padding(5);
+            img({
+                src: "img/ui/" + name + ".png",
+                width: 32,
+                height: 32
+            });
+            onmouseover(function (e) {
+                battleMode.tipBounds = e.target.getBoundingClientRect();
+                return battleMode.tip = function () {
+                    text_align("center");
+                    text(binding);
+                    text(" ");
+                    return span(function () {
+                        color("#f39c12");
+                        return text(settings.humanViewBinding(binding));
+                    });
+                };
+            });
+            return fn();
+        });
+    };
+
+    secondsToString = function(seconds) {
+        var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+        var numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
+        if (JSON.stringify(numminutes).length == 1) numminutes = "0" + numminutes;
+        if (JSON.stringify(numseconds).length == 1) numseconds = "0" + numseconds;
+        return numminutes + ":" + numseconds;
+    }
+
+    draw_miniMap = function () {
+        div(function () {
+            position("absolute");
+
+            bottom(126 + (window.innerHeight / 2) * `0.${Math.round(sim.map_hight)}`);
+            left(0);
+
+            width(96);
+            height(32);
+
+            background("rgba(0,0,0,.2)");
+            border_radius("0px 10px 0px 0px");
+
+            text_align("center");
+            font_size(30);
+            color("white");
+            if (sim.state == "running") text(secondsToString(Math.round(sim.step / 16)));
+            else text("00:00");
+        });
+        return div(function () {
+            position("absolute");
+
+            left(0);
+            bottom(126);
+
+            z_index('2');
+
+            width((window.innerWidth / 2) * `0.${Math.round(sim.map_width)}`);
+            height((window.innerHeight / 2) * `0.${Math.round(sim.map_hight)}`);
+            
+            border("4px solid black");
+            border_color("rgba(0,0,0,.2)");
+            border_radius("0px 10px 0px 0px");
+
+            background_color("rgba(0,0,0,.1)");
+
+            return div(function () {
+
+                position("absolute");
+
+                width(0);
+                height(0);
+                left("50%");
+                top("50%");
+                transform("translate(-50%, -50%)");
+
+                for (let i in sim.things) {
+                    thing = sim.things[i];
+                    if (thing.spawn) {
+                        if (thing.side == "alpha") {
+                            blip(thing.pos, 32, "spawn", "white");
+                        } else if (thing.side == "beta") {
+                            sim.map_width = thing.pos[0];
+                            sim.map_hight = thing.pos[1];
+                            blip(thing.pos, 32, "spawn", "black");
+                        }
+                    } else if (thing.name === "CommandPoint" || thing.capping == 0) {
+                        if (thing.side == "alpha") blip(thing.pos, 16, "commandPoint", "rgba(255,255,255,.2)");
+                        else if (thing.side == "beta") blip(thing.pos, 16, "commandPoint", "rgba(0,0,0,.2)");
+                    } else if (thing.active) {
+                        if (environment.miniMap_type == "radius") {
+                            blip(thing.pos, 10 * ((thing.radius * 2) / 300), "ship", `rgba(${thing.color[0]},${thing.color[1]},${thing.color[2]},255)`)
+                        } else if (environment.miniMap_type == "cost") {
+                            blip(thing.pos, 10 * ((thing.cost * 2) / 2000), "ship", `rgba(${thing.color[0]},${thing.color[1]},${thing.color[2]},255)`)
+                        }
+                    };
+                }
+            });
+        });
+    };
+
+    blip = function (pos, size, type, teamColor) {
+        return div(function () {
+            position("absolute");
+            left(pos[0] / 40);
+            bottom(pos[1] / 40);
+            z_index("3");
+            return div(function () {
+                position("absolute");
+                width(size);
+                height(size);
+
+                left("50%");
+                top("50%");
+                transform("translate(-50%, -50%)");
+                if (type == "spawn") {
+                    border("2px dashed " + teamColor);
+                } else if (type == "player mouse") {
+                    border("2px dashed black");
+                    background_color(teamColor)
+                } else {
+                    background_color(teamColor)
+                }
+                border_radius(`${size}px ${size}px ${size}px ${size}px`);
+            });
+        });
+    };
+
+    draw_valueTracker = function () {
+        return div(function () {
+            position("absolute");
+
+            top(64);
+            left("50%");
+            transform("translate(-50%, 0)");
+
+            width(384);
+            height(224);
+
+            background("rgba(0,0,0,.2)");
+            border_radius("0px 0px 10px 10px");
+        });
+    };
+
+    try {
+        window_body_orig = window.body;
+        window_onkeydown_orig = window.onkeydown;
+    } catch (e) {}
+
+    window.body = e => {
+        if (ui.mode === "battle") {
+            if (environment.miniMap_enabled) draw_miniMap();
+            if (environment.valueTracker_enabled) draw_valueTracker();
+
+            miniButton("aoe", "Mini Map", function () {
+                z_index("2");
+                bottom(84);
+                left((window.innerWidth / 2) - 266);
+                if (environment.miniMap_enabled) {
+                    background("rgba(255,255,255,.3)");
+                }
+                return onclick(function () {
+                    return environment.miniMap_enabled = !environment.miniMap_enabled;
+                });
+            });
+        }
+        return window_body_orig.call(this, e);
+    };
+
+    window.DEFAULT_SETTINGS["Mini Map"] = {
+        keys: [{
+            which: 77
+        }, null]
+    }
+
+    window.onkeydown = e => {
+        if (e.target.type === "text" || e.target.type === "password" || e.target.nodeName === "TEXTAREA") return;
+        else if (settings.key(e, "Mini Map") && ui.mode === "battle") {
+            environment.miniMap_enabled = !environment.miniMap_enabled;
+        }
+        return window_onkeydown_orig.call(this, e);
+    };
+
+    setInterval(() => {
+        if (ui.mode === "battle") onecup.refresh();
+        if (sim.theme !== undefined) mapColor_RGB = JSON.stringify(sim.theme.spotColor).replace(`[`, `rgb(`).slice(0, -5).concat(`)`);; 
+    }, (sim.ticksPerSec + 1) * 2);
+
+}).call(this);
